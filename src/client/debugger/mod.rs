fb24a9aa5ce8394605cf120ada0329fe91023e95
Nico <colete_nico@yahoo.com>
2022-05-16 21:05:44 +0800

    Added onscreen text for player position

--- src/client/debugger/mod.rs
+++ src/client/debugger/mod.rs
@@ -1,6 +1,6 @@
 use bevy::{prelude::*, diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin}};
 
-use super::terrain::TerrainChunk;
+use super::{terrain::TerrainChunk, char::Character};
 
 pub struct CustomPlugin;
 impl Plugin for CustomPlugin {
@@ -8,8 +8,10 @@ impl Plugin for CustomPlugin {
     app
       .add_startup_system(setup_text)
       .add_startup_system(mesh_count_text)
+      .add_startup_system(player_pos_text)
       .add_system(update_text)
-      .add_system(update_mesh_count);
+      .add_system(update_mesh_count)
+      .add_system(update_player_pos);
   }
 }
 
@@ -81,6 +83,39 @@ fn mesh_count_text(mut commands: Commands, asset_server: Res<AssetServer>) {
   .insert(MeshCountText);
 }
 
+fn player_pos_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+  commands.spawn_bundle(UiCameraBundle::default());
+
+  commands.spawn_bundle(TextBundle {
+    style: Style {
+      align_self: AlignSelf::FlexStart,
+      position_type: PositionType::Absolute,
+      position: Rect {
+        top: Val::Px(45.0),
+        left: Val::Px(15.0),
+        ..default()
+      },
+      ..default()
+    },
+    // Use the `Text::with_section` constructor
+    text: Text::with_section(
+      // Accepts a `String` or any type that converts into a `String`, such as `&str`
+      "Mesh count",
+      TextStyle {
+        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+        font_size: 20.0,
+        color: Color::WHITE,
+      },
+      // Note: You can use `Default::default()` in place of the `TextAlignment`
+      TextAlignment {
+        horizontal: HorizontalAlign::Center,
+        ..default()
+      },
+    ),
+    ..default()
+})
+  .insert(PlayerPosText);
+}
 
 
 fn update_text(
@@ -121,6 +156,22 @@ fn update_mesh_count(
   }
 }
 
+fn update_player_pos(
+  mut query: Query<&mut Text, With<PlayerPosText>>,
+  char_query: Query<(&Transform, &Character)>,
+) {
+
+  let mut pos = Vec3::ZERO;
+  for (transform, char) in char_query.iter() {
+    pos = transform.translation;
+  }
+
+  for mut text in query.iter_mut() {
+    // text.sections[0].value = count.to_string();
+    text.sections[0].value = format!("Pos: {:?}", pos);
+  }
+}
+
 
 
 #[derive(Component)]
@@ -128,3 +179,6 @@ struct FpsText;
 
 #[derive(Component)]
 struct MeshCountText;
+
+#[derive(Component)]
+struct PlayerPosText;
